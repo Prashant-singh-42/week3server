b119e15fb860bd2461c18f1073b356fc4f5477de
prashant <prashant292singh@gmail.com>
2024-07-06 17:10:39 +0530

    final2

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173','https://resplendent-figolla-87742e.netlify.app/'],
   credentials: true,
 }));
 app.use(cookieParser());
